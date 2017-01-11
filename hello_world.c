0514b479219e5b0d71da29e0c20eae30423e4071
Ahmad Jarara <ajarara@jarmac.org>
2017-01-11 13:37:12 -0500

    add hello_world.c, fix obnoxious cat size

---
+++ hello_world.c
@@ -0,0 +1,13 @@
+/* header */
+#include <stdio.h>
+
+/* OT: every time I think about how pretty this color scheme is, I think of this post:
+
+https://groups.google.com/d/msg/golang-nuts/hJHCAaiL0so/kG3BHV6QFfIJ
+
+*/
+
+int main(int argc, char** argv) {
+  puts("Hello world!");
+  return 0;
+}
