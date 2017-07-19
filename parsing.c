d3110eb4c0541149ab32091e105a6c5e99fd9d71
Ahmad Jarara <ajarara@jarmac.org>
2017-07-19 12:38:00 -0400

    Add ast representation

--- parsing.c
+++ parsing.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mpc.h"
 
 #include <editline/readline.h>
 /* #include <editline/history.h> */
 
 int main(int argc, char** argv) {
+  mpc_parser_t* Number    = mpc_new("number");
+  mpc_parser_t* Operator  = mpc_new("operator");
+  mpc_parser_t* Expr      = mpc_new("expr");
+  mpc_parser_t* Lispy     = mpc_new("lispy");
+  
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "\
+    number   : /-?[0-9]+/ ; \
+    operator : '+' | '-' | '*' | '/' | '%' ; \
+    expr     : <number> | '(' <operator> <expr>+ ')' ; \
+    lispy    : /^/ <operator> <expr>+ /$/ ; \
+  ", Number, Operator, Expr, Lispy);
+  
   puts("Lispy Version 0.0.0.2");
   puts("Press C-c to exit");
 
@@ -13,10 +27,20 @@ int main(int argc, char** argv) {
 
     add_history(input);
 
-    printf("Here's yer input: %s\n", input);
+    mpc_result_t r;
+    /* So *this derefences a pointer when not in a statement.
+       What does &this do? */
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
 
     free(input);
   }
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
   return 0;
 }
