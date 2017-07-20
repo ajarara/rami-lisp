935321bc0071b04f5e5e2ce0cd635c67910ceeea
Ahmad Jarara <ajarara@jarmac.org>
2017-07-20 14:54:30 -0400

    printing sexprs
    squashing compiler errors is so satisfying

--- parsing.c
+++ parsing.c
@@ -7,102 +7,198 @@
 #include <readline/history.h>
 /* #include <editline/history.h> */
 
-typedef struct {
-  int type;
-  int num;
-  int err;
-} lval;
 
 
-enum { LVAL_NUM, LVAL_ERR };
-
+enum { LVAL_NUM, LVAL_ERR, LVAL_SYM, LVAL_SEXPR };
 enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
 
+/* notice the initial reference to lval. What does that do? */
+typedef struct lval {
+  int type;
+  long num;
+  char* err;
+  char* sym;
+  /* reference counting? */
+  int count;
+  /* is the struct keyword necessary here? */
+  struct lval** cell;
+} lval;
 
-lval lval_num(long x) {
-  lval v;
-  v.type = LVAL_NUM;
-  v.num = x;
+lval* lval_num(long x) {
+  lval* v = malloc(sizeof(lval));
+  /* notice we're no longer using the dot notation. Why? */
+  v->type = LVAL_NUM;
+  v->num = x;
   return v;
 }
 
-lval lval_err(int x) {
-  lval v;
-  v.type = LVAL_ERR;
-  v.err = x;
+lval* lval_err(char* m) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_ERR;
+  v->err = malloc(strlen(m) + 1);
+  /* is the reason we don't alias the pointers to make it conform with v->err? */
+  strcpy(v->err, m);
   return v;
 }
 
 
-lval eval_op(lval x, char* op, lval y) {
-  
-  if (x.type == LVAL_ERR) { return x; }
-  if (y.type == LVAL_ERR) { return y; }
+lval* lval_sym(char* s) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_SYM;
+  v->sym = malloc(strlen(s) + 1);
+  strcpy(v->sym, s);
+  return v;
+}
 
-  /* can't do cases on strs? */
-  if (strcmp(op, "+") == 0) { return lval_num(x.num + x.num); }
-  if (strcmp(op, "-") == 0) { return lval_num(x.num - x.num); }
-  if (strcmp(op, "*") == 0) { return lval_num(x.num * x.num); }
-  if (strcmp(op, "/") == 0) {
-    if (y.num == 0) {
-      return lval_err(LERR_DIV_ZERO);
-    } else {
-      return lval_num(x.num / y.num);
+lval* lval_sexpr(void) {
+  lval* v = malloc(sizeof(lval));
+  v->type = LVAL_SEXPR;
+  v->count = 0;
+  /* Why bother initializing an sexpr's cell? */
+  v->cell = NULL;
+  return v;
+}
+
+
+void lval_del(lval* v) {
+  switch (v->type) {
+  case LVAL_NUM:
+    break;
+  case LVAL_ERR:
+    free(v->err); break;
+  case LVAL_SYM:
+    free(v->sym); break;
+  case LVAL_SEXPR:
+    for (int i = 0; i > v->count; i++) {
+      lval_del(v->cell[i]);
     }
+    free(v->cell);
+    break;
   }
 
-  return lval_err(LERR_BAD_OP);
+  free(v);
 }
 
 
-  
+lval* lval_read_num(mpc_ast_t* t) {
+  errno = 0;
+  long x = strtol(t->contents, NULL, 10);
+  return errno != ERANGE ?
+    lval_num(x) : lval_err("invalid number");
+}
 
-lval eval(mpc_ast_t* t) {
-  if (strstr(t->tag, "number")) {
-    errno = 0;
-    long x = strtol(t->contents, NULL, 10);
-    return errno != ERANGE ? lval_num(x): lval_err(LERR_BAD_NUM);
+
+/* Is this reference counting? what's with the third line?  */
+lval* lval_add(lval* v, lval* x) {
+  v->count++;
+  v->cell = realloc(v->cell, sizeof(lval*) * v->count);
+  v->cell[v->count-1] = x;
+  return v;
+}
+
+
+lval* lval_read(mpc_ast_t* t) {
+  if (strstr(t->tag, "number")) { return lval_read_num(t); }
+  if (strstr(t->tag, "symbol")) { return lval_sym(t->contents); }
+
+  lval* x = NULL;
+
+  /* root? like the prompt? kinda hacky */
+  if (strcmp(t->tag, ">") == 0) { x = lval_sexpr(); }
+  if (strstr(t->tag, "sexpr")) { x = lval_sexpr(); }
+  
+  /* 
+     For the rest of the ast, recurse on the children, excepting parens.
+     What is this regex tag?
+   */
+  
+  for (int i = 0; i < t->children_num; i++) {
+    if (strcmp(t->children[i]->contents, "(") == 0) { continue; }
+    if (strcmp(t->children[i]->contents, ")") == 0) { continue; }
+    if (strcmp(t->children[i]->tag, "regex") == 0) { continue; }
+    x = lval_add(x, lval_read(t->children[i]));
   }
-  /* Everything else in our language so far is an expression */
-
-  
-  char* op = t->children[1]->contents;
-  lval x = eval(t->children[2]);
-  
-
-  int i = 3;
-  while(strstr(t->children[i]->tag, "expr")) {
-    x = eval_op(x, op, eval(t->children[i]));
-    i++;
-  }
-    
   return x;
 }
 
-  
-void lval_print(lval v) {
-  switch (v.type) {
-    /* since it's a number just print it normally */
+void lval_print(lval* v);
+
+void lval_expr_print(lval* v, char open, char close) {
+  putchar(open);
+  for (int i = 0; i < v->count; i++) {
+    lval_print(v->cell[i]);
+    if (i != (v->count-1)) {
+      putchar(' ');
+    }
+  }
+  putchar(close);
+}
+
+void lval_print(lval* v) {
+  switch(v->type) {
   case LVAL_NUM:
-    printf("%d", v.num);
+    printf("%li", v->num);
     break;
   case LVAL_ERR:
-    switch (v.err) {
-    case LERR_DIV_ZERO:
-      printf("Error: Division By Zero");
-      break;
-    case LERR_BAD_OP:
-      printf("Error: Invalid Operator!");
-      break;
-    case LERR_BAD_NUM:
-      printf("Error: Invalid Number!");
-      break;
-    }
+    printf("Error: %s", v->err);
+    break;
+  case LVAL_SYM:
+    printf("%s", v->sym);
+    break;
+  case LVAL_SEXPR:
+    /* recurse */
+    lval_expr_print(v, '(', ')');
     break;
   }
 }
 
-void lval_println(lval v) {
+
+  
+/* lval eval_op(lval x, char* op, lval y) { */
+/*   if (x.type == LVAL_ERR) { return x; } */
+/*   if (y.type == LVAL_ERR) { return y; } */
+  
+
+/*   /\* can't do cases on strs? *\/ */
+/*   if (strcmp(op, "+") == 0) { return lval_num(x.num + x.num); } */
+/*   if (strcmp(op, "-") == 0) { return lval_num(x.num - x.num); } */
+/*   if (strcmp(op, "*") == 0) { return lval_num(x.num * x.num); } */
+/*   if (strcmp(op, "/") == 0) { */
+/*     if (y.num == 0) { */
+/*       return lval_err(LERR_DIV_ZERO); */
+/*     } else { */
+/*       return lval_num(x.num / y.num); */
+/*     } */
+/*   } */
+
+/*   return lval_err(LERR_BAD_OP); */
+/* } */
+ 
+
+/* lval eval(mpc_ast_t* t) { */
+/*   if (strstr(t->tag, "number")) { */
+/*     errno = 0; */
+/*     long x = strtol(t->contents, NULL, 10); */
+/*     return errno != ERANGE ? lval_num(x): lval_err(LERR_BAD_NUM); */
+/*   } */
+/*   /\* Everything else in our language so far is an expression *\/ */
+
+  
+/*   char* op = t->children[1]->contents; */
+/*   lval x = eval(t->children[2]); */
+  
+
+/*   int i = 3; */
+/*   while(strstr(t->children[i]->tag, "expr")) { */
+/*     x = eval_op(x, op, eval(t->children[i])); */
+/*     i++; */
+/*   } */
+    
+/*   return x; */
+/* } */
+
+  
+void lval_println(lval* v) {
   lval_print(v);
   putchar('\n');
 }
@@ -111,18 +207,19 @@ void lval_println(lval v) {
 
 int main(int argc, char** argv) {
   mpc_parser_t* Number    = mpc_new("number");
-  mpc_parser_t* Operator  = mpc_new("operator");
+  mpc_parser_t* Symbol    = mpc_new("symbol");
+  mpc_parser_t* Sexpr     = mpc_new("sexpr");
   mpc_parser_t* Expr      = mpc_new("expr");
   mpc_parser_t* Lispy     = mpc_new("lispy");
   
   mpca_lang(MPCA_LANG_DEFAULT,
-            "                                                     \
-    number   : /-?[0-9]+/ ;                             \
-    operator : '+' | '-' | '*' | '/' ;                  \
-    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-    lispy    : /^/ <operator> <expr>+ /$/ ;             \
-  ", Number, Operator, Expr, Lispy);
-
+            "                                          \
+    number : /-?[0-9]+/ ;                    \
+    symbol : '+' | '-' | '*' | '/' ;         \
+    sexpr  : '(' <expr>* ')' ;               \
+    expr   : <number> | <symbol> | <sexpr> ; \
+    lispy  : /^/ <expr>* /$/ ;               \
+  ", Number, Symbol, Sexpr, Expr, Lispy);
   puts("Lispy Version 0.0.0.2");
   puts("Press C-c to exit");
 
@@ -133,8 +230,10 @@ int main(int argc, char** argv) {
 
     mpc_result_t r;
     if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      lval result = eval(r.output);
-      lval_println(result);      
+      lval* x = lval_read(r.output);
+      lval_println(x);
+      lval_del(x);
+      
       mpc_ast_delete(r.output);
     } else {
       /* Otherwise Print the Error */
@@ -143,7 +242,7 @@ int main(int argc, char** argv) {
     }
     free(input);
   }
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
+  mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
 
   return 0;
 }
